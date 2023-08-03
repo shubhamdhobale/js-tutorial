6367c6966e1337b1968276ce7bcbdc97cf7352d6
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2023-08-03 08:29:59 +0530

    Add files via upload

---
+++ practiseset_1.js
@@ -0,0 +1,33 @@
+console.log("Hello World")
+
+//Q1
+let a="shubham"
+let b=5
+console.log(a+b)
+
+//Q2
+console.log(typeof(a+b))
+
+//Q3
+const a1={
+  name :"Shubham",
+    div:"TEIT",
+    Address:"manchar"
+  
+}
+console.log(a1)
+
+//Q4
+a1['friend']="abc"
+console.log(a1)
+
+//Q5
+let dict={
+  appreciate:
+"to enjoy something or to understand the value of somebody",
+  beautiful:"very pretty or attractive; giving pleasure to the senses.",
+  colleague:"a person who works at the same place as you."
+  
+}
+
+console.log(dict)
