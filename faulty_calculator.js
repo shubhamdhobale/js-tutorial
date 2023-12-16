c44c427b604031318faf6323a2073904ee901762
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2023-12-16 15:46:28 +0530

    Add files via upload

---
+++ faulty_calculator.js
@@ -0,0 +1,16 @@
+
+        let a = prompt("Enter First number: ");
+        let c = prompt("Enter operation: ");
+        let b = prompt("Enter second number: ");
+
+        let obj = {
+            "+": "-",
+            "*": "+",
+            "-": "/",
+            "/": "**"
+        };
+
+        c = obj[c];
+
+        alert(`The result is ${eval(`${a} ${c} ${b}`)}`);
+   
